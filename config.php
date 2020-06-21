ddcf33c000a430fabf34d1134f17ddb299b3babe
Akin <38221575+akinsource@users.noreply.github.com>
2020-06-21 01:28:27 +0100

    Update config.php

--- config.php
+++ config.php
@@ -4,6 +4,6 @@
 */
 define ('DB_USER', "xgstbfqourfyfc");
 define ('DB_PASSWORD', "50d34ccc7baa6da65898c574bb3689dd8009a5a51c166a13348c02613df5268a");
-define ('DB_DATABASE', "d9oiidgf8b85911");
+define ('DB_DATABASE', "d9oiidgf8b8591");
 define ('DB_HOST', "ec2-3-216-129-140.compute-1.amazonaws.com");
 define ('DB_PORT', "5432");
